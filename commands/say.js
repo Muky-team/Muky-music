05f78855590574d3df4c8ca3e007a6c2d710c5f5
Persea-gatuna <leonel.-s@hotmail.com>
2021-01-22 22:24:59 -0300

    Deleted say.js 🚫
    - Deleted testing command

--- commands/say.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "say",
-    aliases: ["repeat"],
-    execute: async (message, args) => {
-        var text = args.join(' ');
-        if(!text) return message.channel.send(`<@${message.author.id}> Usage: \`mk!say [Text]\``);
-        message.channel.send(text);
-    },
-};
